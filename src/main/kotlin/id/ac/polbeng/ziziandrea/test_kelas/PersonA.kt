9bbebf260024683f1d7321c303ce65f379c7faa4
zizi andrea <ziziandrea@gmail.com>
2022-03-24 11:26:52 +0700

    Pertemuan 3 Zizi Konsep OOP Kotlin PersonA

---
+++ src/main/kotlin/id/ac/polbeng/ziziandrea/test_kelas/PersonA.kt
@@ -0,0 +1,21 @@
+package id.ac.polbeng.ziziandrea.test_kelas
+
+//Parameter dari konstruktor utama dapat digunakan dalam blok inisialisasi. Ia juga dapat
+//digunakan untuk menginisialisasi property yang dideklarasikan dalam tubuh kelas.
+
+class PersonA (_firstName: String, _lastName: String, _age: Int) {
+    var firstName:String
+    var lastName:String
+    var age: Int
+    init {
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }
+}
+
+fun main() {
+    val budi = PersonA("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}

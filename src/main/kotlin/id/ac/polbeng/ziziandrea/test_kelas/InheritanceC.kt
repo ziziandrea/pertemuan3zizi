7b6db68b9a6341b7969ca454712ea8e81b8deb45
zizi andrea <ziziandrea@gmail.com>
2022-03-24 14:40:03 +0700

    Pertemuan 3 Zizi Konsep OOP Kotlin InheritanceC

---
+++ src/main/kotlin/id/ac/polbeng/ziziandrea/test_kelas/InheritanceC.kt
@@ -0,0 +1,25 @@
+package id.ac.polbeng.ziziandrea.test_kelas
+
+open class Teacher {
+    open fun teach() {
+        println("Teaching....")
+    }
+
+    fun info() {
+        println("I'am a Teacher.")
+    }
+}
+
+class MathsTeacher : Teacher() {
+    override fun teach() {
+        println("Teaching Maths...")
+    }
+}
+
+fun main() {
+    val teacher = Teacher()
+    val mathsTeacher = MathsTeacher()
+    mathsTeacher.info()
+    teacher.teach()
+    mathsTeacher.teach()
+}

e917b96dd2f5c28f4697bf537516a4122ef7c72c
zizi andrea <ziziandrea@gmail.com>
2022-03-24 15:17:32 +0700

    Pertemuan 3 Zizi Konsep OOP Kotlin InheritanceD

---
+++ src/main/kotlin/id/ac/polbeng/ziziandrea/test_kelas/InheritanceD.kt
@@ -0,0 +1,40 @@
+package id.ac.polbeng.ziziandrea.test_kelas
+
+import java.text.NumberFormat
+import java.util.*
+
+open class EmployeeD {
+    open  val baseSalary: Int = 3000000
+}
+
+class Programmer : EmployeeD() {
+    override val baseSalary: Int = 5000000
+}
+
+class SoftwareEngineer : EmployeeD() {
+    override val baseSalary: Int = 6000000
+}
+
+class ProjectManager : EmployeeD() {
+    override val baseSalary: Int = 8000000
+}
+
+fun toRupiahFormat(baseSalary: Int) : String{
+    val localeID = Locale("in", "ID")
+    val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+    return numberFormat.format(baseSalary).toString()
+}
+
+fun main() {
+    val employee =  EmployeeD()
+    println("Rata-rata gaji ${employee.javaClass.simpleName} = ${toRupiahFormat(employee.baseSalary)}")
+
+    val programmer = Programmer()
+    println("Rata-rata gaji ${programmer.javaClass.simpleName} = ${toRupiahFormat(programmer.baseSalary)}")
+
+    val softwareEngineer = SoftwareEngineer()
+    println("Rata-rata gaji ${softwareEngineer.javaClass.simpleName} = ${toRupiahFormat(softwareEngineer.baseSalary)}")
+
+    val projectManager = ProjectManager()
+    println("Rata-rata gaji ${projectManager.javaClass.simpleName} = ${toRupiahFormat(projectManager.baseSalary)}")
+}

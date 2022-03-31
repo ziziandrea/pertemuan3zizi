a9584641e79c1465ae9141acfe355584e70b1500
zizi andrea <ziziandrea@gmail.com>
2022-03-31 11:36:33 +0700

    Pertemuan 3 Zizi Konsep OOP Data class toString() method

---
+++ src/main/kotlin/id/ac/polbeng/ziziandrea/test_kelas/DataClassD.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.ziziandrea.test_kelas
+
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString())
+}
+
+//Fungsi toString() pada data kelas mengembalikan nilai kembalian String yang merupakan
+//representasi dari objek.
